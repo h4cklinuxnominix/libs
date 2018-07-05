8a10a43a7ae228870f3631a5dba19b00f0ccfe32
wipedlife <u@u.u>
2018-07-06 01:12:21 +0700

    inject first commit

---
+++ inject/inject.c
@@ -0,0 +1,95 @@
+#include"inject.h"
+
+#define INITED(ret)         if(!attached[pid]) return ret;
+#define SIGTRAP_SINGLESTEP ((5<<12|127<<4)>>4) 
+//127==all ok 126==error 0==not have any
+//
+#define ATTACH_ARR_INITED if(attached==NULL) if(init_attached()==-1)return false;
+
+int init_attached(void){
+	int proc_max=open("/proc/sys/kernel/pid_max", O_RDONLY);
+	if(proc_max==-1){
+		perror("Can't get pid_max");
+		return -1;
+	}
+	char buf[1024];
+	if(read(proc_max,buf, sizeof(buf)) == -1){
+		perror("Read");
+		return -1;
+	}
+	long long max_pid=atoi(buf);
+	attached=calloc(sizeof(bool),max_pid);
+	return 0;
+
+}
+
+bool attach_me(void){
+	ATTACH_ARR_INITED;
+	if(ptrace(PTRACE_TRACEME,0,0,0) == -1)
+	{
+		perror("Attach");
+		return false;
+	}
+	attached[getpid()]=true;
+	return true;
+}
+
+bool attach_pid(pid_t pid){
+	ATTACH_ARR_INITED;
+	if(ptrace(PTRACE_ATTACH, pid,0,0) == -1){
+		perror("Attach");
+		return false;
+	}
+	attached[pid]=true;
+	return true;
+}
+
+bool deattach(pid_t pid){
+	INITED(false);
+
+	bool ok = ptrace(PTRACE_DETACH, pid,0,0) != -1;
+	if(ok)attached[pid]=false;
+	else{
+		perror("Deattach: ");
+		return false;
+	}
+	return true;
+}
+
+bool freeze(pid_t pid){
+	INITED(false);
+	return ptrace(PTRACE_SINGLESTEP,pid,0,0) != -1;
+}
+
+syscall_hack getcall(pid_t pid){
+	syscall_hack err={0,0};
+	INITED(err);
+	struct user_regs_struct regs;
+	if(ptrace(PTRACE_GETREGS,pid, 0, &regs) == -1){
+		perror("Can't getregs");
+		return err;
+	}
+        syscall_hack ret={regs.ax,regs.ip};
+	int status;
+	waitpid(pid,&status,0);
+	if(status!=SIGTRAP_SINGLESTEP){
+		perror("Waitpid");
+	}
+	return ret;
+}
+
+bool putregs(pid_t pid,struct user_regs_struct regs,void *offset){
+	INITED(false);
+	return ptrace(PTRACE_SETREGS,pid,offset,&regs) == -1;
+}
+
+bool putcode(pid_t pid, void * offset, void * data){
+	INITED(false);
+	return ptrace(PTRACE_PEEKTEXT,pid,offset,data) == -1;
+}
+
+bool putdata(pid_t pid, void * offset, void * data){
+	INITED(false);
+	return ptrace(PTRACE_PEEKDATA, pid, offset, data) == -1;
+}
+
--- inject/inject.h
+++ inject/inject.h
@@ -1,2 +1,40 @@
-#include<sys/ptrace.h>
 #include<linux/user.h>
+#include<sys/ptrace.h>
+#include<unistd.h>
+#include<stdbool.h>
+//#include <sys/types.h>
+//#include <sys/stat.h>
+#include <fcntl.h>
+
+#ifdef __cplusplus
+extern "C"{// not c++filt shit
+#endif
+	typedef struct syscall_hack{
+		long long call; //eax
+		long long ip; // instruction index
+	}syscall_hack;
+
+	#define eprintf(...) fprintf(stderr, __VA_LIST__)
+	static bool * attached;
+//	static pid_t attached_pid;
+	int init_attached(void);
+
+	bool attach_me(void);
+	bool attach_pid(pid_t);
+
+	bool freeze(pid_t); // freeze
+	bool (*start_singlestep)(void)=(void*)&freeze;
+	syscall_hack getcall(pid_t );
+	bool (*unfreeze)(void) = (void*)&getcall;
+
+
+	bool putregs(pid_t,struct user_regs_struct,void *offset);
+	bool putcode(pid_t,void * offset, void * data); // put in .text
+	bool putdata(pid_t,void * offset, void * data); // put in .data
+
+	bool inject_lib(pid_t,char pathlib[], char funcname[]);
+
+
+#ifdef __cplusplus
+}
+#endif
